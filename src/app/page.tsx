6843e0c5f8a2d8f812e10938f944890453c2ea83
Vendaarlia <94086273+Vendaarlia@users.noreply.github.com>
2024-03-10 08:06:13 +0700

    nav bar mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,10 @@
 'use client'
 import Contact from "@/components/Contact";
 import Hero1 from "@/components/Hero1";
-import Navbar from "@/components/Navbar";
-import Navbar1 from "@/components/Navbar1";
 import Project from "@/components/Project";
 import Social from "@/components/Social";
 import Team from "@/components/Team";
 import Timeline from "@/components/Timeline";
-import Play from "@/components/Play";
 import { Navbar } from "@/components/Navbar";
 
 
