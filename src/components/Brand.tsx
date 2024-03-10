58b4f25533277fe1c9d7f450dad87449dd042931
Vendaarlia <94086273+Vendaarlia@users.noreply.github.com>
2024-03-10 11:47:38 +0700

    image update nav update

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Brand() {
     return (
         <>
-        <div className="brand absolute w-[50px] h-[50px] top-[18px] left-[100px] md:left-[150px]">
+        <div className="brand absolute w-[500px] h-[50px] top-[18px] left-[100px] md:left-[150px]">
                 <a href="http://">
-                    <Image src="/images/logo.jpg" alt="" width={40} height={40} className="rounded-full" />
+                    <h1 className="font-bold text-2xl">Digital Delivered</h1>
                 </a>
             </div>
         </>
--- src/components/Hero1.tsx
+++ src/components/Hero1.tsx
@@ -1,15 +1,14 @@
 import Image from "next/image"
-import { global } from "styled-jsx/css"
 
 export default function Hero1() {
     return (
         <>
         <section className="hero1 h-screen pb-44 relative">
             <div className="wrap w-screen h-full m-0 top-0 flex justify-center items-center">
-                <div className="absolute top-72"><h1 className="txt-line">DIGITAL. <br />DELIVERED.</h1></div>
-                <Image className="absolute grayscale justify-center align-middle" src="/images/hero1.png" alt="" width={500} height={500} />
-                <div className="absolute top-72"><h1 className="txt-fill">DIGITAL. <br />DELIVERED.</h1></div>
-                <div className="circle absolute top-32 right-[150px] md:right-[520px] h-32 w-32 rounded-full border-[#e14c6c] border-4"></div>
+                <div className="absolute top-60 md:top-72"><h1 className="txt-line text-[7rem] md:text-9xl text-black font-bold text-center leading-none">DIGITAL. <br />DELIVERED.</h1></div>
+                <Image className="absolute grayscale justify-center align-middle" src="/images/hero2.png" alt="" width={900} height={500} />
+                <div className="absolute top-60 md:top-72"><h1 className="txt-fill pt-[0.9rem] pl-[0.9rem] text-[7rem] md:text-9xl text-white font-bold text-center leading-none">DIGITAL. <br />DELIVERED.</h1></div>
+                <div className="circle absolute top-[20.4rem] md:top-[24.2rem] right-[116px] md:right-[444px] h-8 w-8 rounded-full border-[#e14c6c] border-2 "></div>
             </div>
             <div className="selogan text-center">
                 <p>Our message is simple. we have time & again <br /> <span className="font-bold text-[#e14c6c]  ">DELIVERED</span> where others failed.</p>
